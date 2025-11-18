8800f710ec9fa327943426d4e99621d4888f7b8e
Riaru Moda <riarumoda@gmail.com>
2025-11-18 12:50:14 +0700

    partial: login page

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -12,25 +12,25 @@
 		<Card.Title class="text-center text-2xl">Login</Card.Title>
 	</Card.Header>
 	<Card.Content>
-		<form method="POST" action="?/login">
-			<div class="flex flex-col gap-6">
-				<div class="grid gap-2">
-					<Label for="email">Email</Label>
-					<Input id="email" type="email" name="email" placeholder="doctor@example.com" required />
-				</div>
-				<div class="grid gap-2">
-					<div class="flex items-center">
-						<Label for="password">Password</Label>
-						<a href="##" class="ml-auto inline-block text-sm underline-offset-4 hover:underline">
-							Forgot your password?
-						</a>
-					</div>
-					<Input id="password" type="password" name="password" required />
-				</div>
+	<form method="POST" action="?/login">
+		<div class="flex flex-col gap-6">
+			<div class="grid gap-2">
+				<Label for="email">Email</Label>
+				<Input id="email" type="email" name="email" placeholder="doctor@example.com" required />
 			</div>
-		</form>
-	</Card.Content>
-	<Card.Footer class="flex-col gap-2">
-		<Button type="submit" class="w-full">Login</Button>
-	</Card.Footer>
+			<div class="grid gap-2">
+				<div class="flex items-center">
+					<Label for="password">Password</Label>
+					<a href="##" class="ml-auto inline-block text-sm underline-offset-4 hover:underline">
+						Forgot your password?
+					</a>
+				</div>
+				<Input id="password" type="password" name="password" required />
+			</div>
+
+			<Button type="submit" class="w-full mt-4">Login</Button>
+		</div>
+	</form>
+</Card.Content>
+
 </Card.Root>

1d10b3b0cef7a0e5003a1cbfb9756f99f850d6c6
krisnaganesha1609 <igkganes@gmail.com>
2025-11-21 00:47:24 +0700

    feat: added collapsible sidebar (only doctor for now)

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
+	import {
+		CalendarRange,
+		ChevronLeft,
+		ChevronRight,
+		HeartPulse,
+		House,
+		LogOut
+	} from '@lucide/svelte';
+	import logo from '$lib/assets/logo-white.svg';
+	import { page } from '$app/state';
 
 	let { data, children } = $props();
 	let { session, supabase } = $derived(data);
@@ -15,74 +25,116 @@
 	});
 
 	const role = data?.role;
+	let isCollapsed = $state(false);
 </script>
 
-<aside
-	id="default-sidebar"
-	class="fixed top-0 left-0 z-40 h-full w-64 -translate-x-full transition-transform sm:translate-x-0"
-	aria-label="Sidebar"
+<button
+	class="absolute top-4 {isCollapsed
+		? ' translate-x-2'
+		: ' translate-x-60'} z-50 cursor-pointer rounded-full bg-[#1d69d1] p-2 shadow-lg transition-transform duration-300"
+	onclick={() => (isCollapsed = !isCollapsed)}
 >
-	<div class="bg-neutral-primary-soft border-default h-full overflow-y-auto border-e px-3 py-4">
-		<ul class="space-y-2 font-medium">
+	{#if isCollapsed}
+		<ChevronRight color="white" />
+	{:else}
+		<ChevronLeft color="white" />
+	{/if}
+</button>
+<aside
+	class="fixed top-0 {isCollapsed
+		? 'w-min -translate-x-1/12'
+		: 'w-64 translate-x-0'} z-40 h-full transition-transform duration-300"
+>
+	<div class="border-default h-full overflow-y-auto border-e bg-[#113f7d] px-3 py-4">
+		<img src={logo} alt="Logo" srcset="" class="mt-10 mb-10 px-4 {isCollapsed ? 'w-0' : ''}" />
+		<ul class="space-y-5 font-medium">
 			{#if role === 'Doctor'}
-				<li>
+				<li
+					class={page.url.pathname.includes('/app/dashboard/doctor')
+						? 'rounded-[20px] bg-[#e8f0fa] py-2'
+						: ''}
+				>
 					<a
 						href="/app/dashboard/doctor"
-						class="text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group flex items-center px-2 py-1.5"
+						class="text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group {isCollapsed
+							? 'justify-center'
+							: 'pl-4'} flex items-center px-2 py-1.5"
 					>
-						<svg
-							class="group-hover:text-fg-brand h-5 w-5 transition duration-75"
-							aria-hidden="true"
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							fill="none"
-							viewBox="0 0 24 24"
-							><path
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M10 6.025A7.5 7.5 0 1 0 17.975 14H10V6.025Z"
-							/><path
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M13.5 3c-.169 0-.334.014-.5.025V11h7.975c.011-.166.025-.331.025-.5A7.5 7.5 0 0 0 13.5 3Z"
-							/></svg
-						>
-						<span class="ms-3">Dashboard</span>
+						<House
+							color={page.url.pathname.includes('/app/dashboard/doctor') ? '#1754a7' : 'white'}
+						/>
+						{#if !isCollapsed}
+							<span
+								class="ms-3 {page.url.pathname.includes('/app/dashboard/doctor')
+									? ' text-[#1754a7]'
+									: 'text-white'}">Dashboard</span
+							>
+						{/if}
 					</a>
 				</li>
-				<li>
+				<li
+					class={page.url.pathname.includes('/app/patients')
+						? 'rounded-[20px] bg-[#e8f0fa] py-2'
+						: ''}
+				>
 					<a
 						href="/app/patients"
-						class="text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group flex items-center px-2 py-1.5"
+						class="text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group flex items-center px-2 py-1.5 {isCollapsed
+							? 'justify-center'
+							: 'pl-4'} "
 					>
-						<svg
-							class="group-hover:text-fg-brand h-5 w-5 transition duration-75"
-							aria-hidden="true"
-							xmlns="http://www.w3.org/2000/svg"
-							width="24"
-							height="24"
-							fill="none"
-							viewBox="0 0 24 24"
-							><path
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M10 6.025A7.5 7.5 0 1 0 17.975 14H10V6.025Z"
-							/><path
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M13.5 3c-.169 0-.334.014-.5.025V11h7.975c.011-.166.025-.331.025-.5A7.5 7.5 0 0 0 13.5 3Z"
-							/></svg
-						>
-						<span class="ms-3">Daftar Pasien</span>
+						<HeartPulse color={page.url.pathname.includes('/app/patients') ? '#1754a7' : 'white'} />
+						{#if !isCollapsed}
+							<span
+								class="ms-3 {page.url.pathname.includes('/app/patients')
+									? ' text-[#1754a7]'
+									: 'text-white'}">Patients</span
+							>
+						{/if}
+					</a>
+				</li>
+				<li
+					class={page.url.pathname.includes('/app/reservations/doctor')
+						? 'rounded-[20px] bg-[#e8f0fa] py-2'
+						: ''}
+				>
+					<a
+						href="/app/reservations/doctor"
+						class="text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group flex items-center px-2 py-1.5 {isCollapsed
+							? 'justify-center'
+							: 'pl-4'} "
+					>
+						<CalendarRange
+							color={page.url.pathname.includes('/app/reservations/doctor') ? '#1754a7' : 'white'}
+						/>
+						{#if !isCollapsed}
+							<span
+								class="ms-3 {page.url.pathname.includes('/app/reservations/doctor')
+									? ' text-[#1754a7]'
+									: 'text-white'}">View Reservations</span
+							>
+						{/if}
+					</a>
+				</li>
+				<li
+					class={page.url.pathname.includes('/app/logout')
+						? 'rounded-[20px] bg-[#e8f0fa] py-2'
+						: ''}
+				>
+					<a
+						href="/app/logout"
+						class="text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group flex items-center px-2 py-1.5 {isCollapsed
+							? 'justify-center'
+							: 'pl-4'} "
+					>
+						<LogOut color={page.url.pathname.includes('/app/logout') ? '#1754a7' : 'white'} />
+						{#if !isCollapsed}
+							<span
+								class="ms-3 {page.url.pathname.includes('/app/logout')
+									? ' text-[#1754a7]'
+									: 'text-white'}">Log Out</span
+							>
+						{/if}
 					</a>
 				</li>
 			{/if}
@@ -185,7 +237,7 @@
 		</ul>
 	</div>
 </aside>
-<div class="p-4 sm:ml-64">
+<div class="p-4 {isCollapsed ? 'ml-12' : 'ml-64'} transition-all duration-300">
 	<div class="border-default rounded-base border border-dashed p-4">
 		{@render children?.()}
 	</div>

65af87da885cde78dac7cc7968018481e20cbb41
krisnaganesha1609 <igkganes@gmail.com>
2025-12-22 15:18:06 +0700

    feat: added more pagination store

--- src/lib/shared/stores/pagination.ts
+++ src/lib/shared/stores/pagination.ts
@@ -1,11 +1,26 @@
 import { writable } from "svelte/store";
 
-export const patientPaginationStore = writable<{ currentPage: number; itemsPerPage: number }>({
+export const patientPaginationStore = writable<{ currentPage: number; itemsPerPage: number}>({
     currentPage: 1,
     itemsPerPage: 7,
 });
 
 export const reservationPaginationStore = writable<{ currentPage: number; itemsPerPage: number }>({
+    currentPage: 1,
+    itemsPerPage: 7,
+});
+
+export const prescriptionPaginationStore = writable<{ currentPage: number; itemsPerPage: number }>({
+    currentPage: 1,
+    itemsPerPage: 7,
+});
+
+export const medicinePaginationStore = writable<{ currentPage: number; itemsPerPage: number }>({
+    currentPage: 1,
+    itemsPerPage: 7,
+});
+
+export const userPaginationStore = writable<{ currentPage: number; itemsPerPage: number }>({
     currentPage: 1,
     itemsPerPage: 7,
 });

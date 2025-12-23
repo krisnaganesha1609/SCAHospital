0e70e57bd06fa70f732f7691d1f1c94caa926560
krisnaganesha1609 <igkganes@gmail.com>
2025-12-23 13:24:47 +0700

    delete: departments

--- src/lib/server/repositories/interfaces/DepartmentsRepository.ts
+++
@@ -1,9 +0,0 @@
-import type { uuid } from '$lib/shared/types/type_def';
-import { Departments } from '../../../shared/entities/Departments';
-export abstract class DepartmentsRepository {
-    abstract createDepartments(departments: Departments): Promise<void>;
-    abstract findAll(): Promise<any[]>;
-    abstract findById(id: uuid): Promise<any>;
-    abstract update(id: uuid, departments: Partial<Departments>): Promise<void>;
-    abstract delete(id: uuid): Promise<void>;
-}
